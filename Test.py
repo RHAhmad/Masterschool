18a62473fd7cc8140a30a08cfbca72354eb7e3b5
RHAhmad <rhahmad1980@googlemail.com>
2024-10-08 22:53:22 +0200

    First File from PyCharm to GitHub

--- Test.py
+++ Test.py
@@ -1,4 +1,4 @@
 '''
 First Test File from PyCharm to GitHub
 '''
-print("Hello this is the  first Test going from PyCharm to GitHub")
+print("Hello this is the first Test going from PyCharm to GitHub")
